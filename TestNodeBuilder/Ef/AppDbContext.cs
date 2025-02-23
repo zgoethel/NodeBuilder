5bbcd5776aaeb2d35bd4c74eb0f1fe4cc564e00a
Zach Goethel <13502112+zgoethel@users.noreply.github.com>
2025-02-23 00:28:49 -0600

    Start adding EF

---
+++ TestNodeBuilder/Ef/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestNodeBuilder.Models;
+
+namespace TestNodeBuilder.Ef;
+
+public class AppDbContext : DbContext
+{
+    public const string DB_FILE = "data.db";
+
+    public DbSet<Grammar> Grammars { get; set; }
+    public DbSet<TokenGroup> TokenGroups { get; set; }
+    public DbSet<Token> Tokens { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DB_FILE}");
+}
---
+++ TestNodeBuilder/Models/Grammar.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestNodeBuilder.Models;
+
+[Table("Grammar")]
+[PrimaryKey("Id")]
+public class Grammar
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public DateTime? Deleted { get; set; }
+}
+
+[Table("TokenGroup")]
+[PrimaryKey("Id")]
+public class TokenGroup
+{
+    public int Id { get; set; }
+    public int GrammarId { get; set; }
+    public string Name { get; set; } = "";
+    public DateTime? Deleted { get; set; }
+}
+
+[Table("Token")]
+[PrimaryKey("Id")]
+public class Token
+{
+    public int Id { get; set; }
+    public int GrammarId { get; set; }
+    public int? TokenGroupId { get; set; }
+    public string Name { get; set; } = "";
+    public string Regex { get; set; } = "";
+    public int OrderIndex { get; set; }
+    public DateTime? Deleted { get; set; }
+}
